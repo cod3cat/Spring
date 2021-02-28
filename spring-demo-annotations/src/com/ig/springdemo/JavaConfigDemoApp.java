74f48eca4d14612c3a67a508378aac0e35cb4c80
Praveen G <25690316+cod3cat@users.noreply.github.com>
2021-02-28 16:14:53 +0530

    Try adding beans to annotationContext using java classs

---
+++ spring-demo-annotations/src/com/ig/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,24 @@
+package com.ig.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read spring config file
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(SportConfig.class);
+		
+		// get the bean from spring container
+		Coach theCoach = context.getBean("tennisCoach_DefaultBeanID", Coach.class);
+		
+		// call a method on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		
+		// close the context
+		context.close();
+	}
+
+}
